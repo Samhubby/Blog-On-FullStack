8d60224351865e8fd340d31a7df4ccac00b1b3ba
@Samhubby <samayaasylumml@gmail.com>
2024-08-20 12:01:53 +0545

    chore: Update proxy URL for API requests

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": 'https://fullstack-3xjz.onrender.com',
+      "/api": {
+        target: "https://fullstack-3xjz.onrender.com",
+      },
     },
   },
   plugins: [react()],
